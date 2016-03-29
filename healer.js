2c5d9179299b1c326260fda58dc01eb6eb443432
David Hargat <davidhargat@yahoo.com>
2016-03-28 20:26:57 -0400

    healer

--- healer.js
+++ healer.js
@@ -1,12 +1,39 @@
 var base = require("base");
 
+var findHealTarget = function(creep){
+	var spawn = base.getBase();
+	
+	var target = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+		filter: function(obj){
+			return 
+				(obj != creep) &&
+				(obj.hits < obj.hitsMax);
+		}
+	});
+
+	return target;
+};
+
+var healTarget = function(creep, target){
+
+	var result = creep.heal(target);
+	
+	if(result != OK){
+		if(result == ERR_NOT_IN_RANGE){
+			creep.moveTo(target);
+		}else{
+			creep.say("err?");
+		}
+	}
+};
+
 var logic = function(creep){
 	
-	var target = findTarget();
+	var target = findHealTarget(creep);
 	var spawn = base.getBase();
 
-	if(target != false){
-		healTarget(target);
+	if(target != null){
+		healTarget(creep, target);
 	}else{
 		creep.moveTo(spawn);
 	}
